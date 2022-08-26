916b6dd45dced4804622eaa0d14c80b4c7f299a0
Joseph Mariga <jsphmariga@gmail.com>
2022-08-26 02:50:18 +0000

    Depth

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -7,17 +7,10 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	int l_height;
-	int r_height;
-
 	if (tree == NULL)
 		return (0);
 
-	l_height = binary_tree_depth(tree->left);
-	r_height = binary_tree_depth(tree->right);
-
-	if (l_height > r_height)
-		return (l_height + 1);
-	else
-		return (r_height + 1);
+	if (tree && tree->parent)
+		return (1 + binary_tree_depth(tree->parent));
+	return (0);
 }
